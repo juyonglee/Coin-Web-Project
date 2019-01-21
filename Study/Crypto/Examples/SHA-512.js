bf99d6244fc627ba9bb249cafd1a0b9efbb402e4
juyonglee <juyonglee0208@gmail.com>
2019-01-21 09:56:02 +0900

    SHA-512를 이용한 예제 추가

---
+++ Study/Crypto/Examples/SHA-512.js
@@ -0,0 +1,9 @@
+  //  암호화를 위한 모듈 추가 w
+  const crypto = require('crypto');
+  //  sha512기반의 hash 암호화 
+  const hash = crypto.createHash('sha512');
+  hash.update("User Input Password", 'utf-8');
+  console.log("[Original Input]");
+  console.log("User Input Password");
+  console.log("[SHA-512 Hash]");
+  console.log(hash.digest('base64'));
