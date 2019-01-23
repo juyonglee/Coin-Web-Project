7b5d63896ece37e6b7c726d13ad6f5b40d7d711f
JUNGEEYOU <junge2u@naver.com>
2019-01-24 00:21:57 +0900

    관리자 페이지 만들기

--- COINPRA/app.js
+++ COINPRA/app.js
@@ -26,6 +26,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var accountRouter = require('./routes/account');
 var productRouter = require('./routes/product');
+var managerRouter = require('./routes/manager');
 
 var app = express();
 
@@ -53,6 +54,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/account', accountRouter);
 app.use('/product', productRouter);
+app.use('/manager', managerRouter);
 
 //  Passport Configure
 passport.use(new LocalStrategy(function(username, password, done){
