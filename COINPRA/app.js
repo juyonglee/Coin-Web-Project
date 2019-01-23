8cf5d05b93cb5a86ffd31191b82952b7e07cf2d9
juyonglee <juyonglee0208@gmail.com>
2019-01-23 16:01:07 +0900

    Login을 위한 passport 및 mongoDB 설정

--- COINPRA/app.js
+++ COINPRA/app.js
@@ -4,6 +4,25 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//  Passport Setting 추가
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+//  MongoDB 
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise; 
+var AutoIncrement = require('mongoose-sequence')(mongoose);
+var mongoUrl = 'mongodb://localhost:27017/PROJECT';
+mongoose.connect(mongoUrl, {useMongoClient: true});
+var db = mongoose.connection;
+db.once('open', function(){
+  console.log("MongoDB Connection!");
+});
+
+//  Model 추가
+var Account = require('./models/account');
+
+//  Router Loading
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -19,6 +38,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//  Passport Setup & Seesion 사용을 위한 express-session 추가
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+//  Router Setting
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
