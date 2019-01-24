d82af1326c70e25f8b5a020ab63ee121cbaeccbd
juyonglee <juyonglee0208@gmail.com>
2019-01-24 14:38:51 +0900

    회원관리 기능 수정 및 정리

--- COINPRA/routes/account.js
+++ COINPRA/routes/account.js
@@ -4,6 +4,7 @@ var passport = require('passport');
 var Account = require('../models/account');
 const crypto = require('crypto');
 
+//  회원 가입에 관련된 Router (GET, POST)
 router.get('/signup', function(req, res, next) {
     res.render('signup', {});
 });
@@ -11,31 +12,34 @@ router.get('/signup', function(req, res, next) {
 router.post('/signup', function(req, res, next) {
     crypto.randomBytes(64, (err, buf) => {
         crypto.pbkdf2(req.body.password, buf.toString('base64'), 100000, 64, 'sha512', function(err, derivedKey) {
-        Account.create({ username : req.body.username, salt:buf.toString('base64'), phone: req.body.phoneNumber, password: derivedKey.toString('base64'), name: req.body.name, FNB: 0}).then(
+        Account.create({ username : req.body.username, salt:buf.toString('base64'), phone: req.body.phoneNumber, password: derivedKey.toString('base64'), name: req.body.name}).then(
             function() {
-                console.log("저장");
+                //  저장이 완료된 경우 호출
                 passport.authenticate('local')(req, res, function () {
                     res.redirect('/');
                 });
             }, function(err) {
-                console.log("이미 가입된 회원입니다.");
+                //  이미 가입된 경우 호출
                 res.redirect('/');
             });
         });
     });
 });
 
+//  로그인에 관련된 Router (GET, POST)
 router.get('/signin', function(req, res, next) {
     res.render('signin', { title: 'Express' });
 });
 
 router.post('/signin', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/account/signin' }));
 
+//  로그아웃에 관련된 Router (GET)
 router.get('/logout', function(req, res, next) {
     req.logout();
     res.redirect('/');
 });
 
+//  비밀번호 변경에 관련된 Router (GET, POST)
 router.get('/changePassword', function(req, res, next) {
     if(req.user) {
         res.render('changePassword', { });
