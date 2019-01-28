84ad8c495bc2a0c55e6c473e6a6389be42098937
JUNGEEYOU <junge2u@naver.com>
2019-01-28 22:39:56 +0900

    구매 페이지 1-3 추가

--- COINPRA/routes/saleList.js
+++ COINPRA/routes/saleList.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
     res.render('saleList', { title: "test"});
 });
 
+// 1-3 페이지
+router.get('/detail', function(req, res, next) {
+    res.render('saleDetail', { title: "test"});
+});
 module.exports = router;
