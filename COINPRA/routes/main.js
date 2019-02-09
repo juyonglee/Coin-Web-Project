22169d06fbe5f48a625dd4f829f726082266b325
juyonglee <juyonglee0208@gmail.com>
2019-02-09 21:51:20 +0900

    판매 기록을 위한 router 구현

--- COINPRA/routes/main.js
+++ COINPRA/routes/main.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('test', { title: "test"});
+    // res.render('test', { title: "test"});
+    if(req.user) {
+    res.render('index', { title: "안녕하세요, " + req.user.username + "님"});
+  } else {
+    res.render('index', { title: 'Express'});
+  }
 });
 
 module.exports = router;
--- COINPRA/routes/saleList.js
+++ COINPRA/routes/saleList.js
@@ -1,13 +1,63 @@
 var express = require('express');
 var router = express.Router();
 
+var Account = require('../models/account');
+var BuyInfo = require('../models/buyInfo');
+var moment = require('moment');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('saleList', { title: "test"});
+    if(req.user) {
+        res.render('saleList', { title: "test"});
+    } else {
+        res.redirect(`/account/signin`);
+    }
 });
 
 // 1-3 페이지
-router.get('/detail', function(req, res, next) {
-    res.render('saleDetail', { title: "test"});
+router.get('/detail', function(req, res, next) {    
+    // if(req.user) {
+        // console.log(req.user.username);
+        //req.user.username
+        Account.findOne({username: 'goal0208@naver.com'}).populate({path:'buy_info', model:BuyInfo}).exec((err, member) => {
+            if(err) {
+                console.log("User가 존재하지 않습니다.");
+                next(err);
+            } else {
+                // [유정이] - Property와 변수의 이름이 같으면 문제가 생깁니다.
+                // const member ={sequence: req., name: "jungee", email: "junge2",phone:"0000", plu:0, fnb: 1};
+                //  [유정이] - Hover로 부탁드려요!
+                console.log(req.query.coin);
+                console.log("User가 존재합니다.");
+                console.log(member);
+                res.render('saleDetail', { buyInfo: member.buy_info, moment});
+            }
+        }); 
+    // } else {
+        // res.redirect(`/account/signin`);
+    // }
+});
+
+router.post('/depositDelete/:user_id', function(req, res, next) {
+    BuyInfo.findOne({_id:req.params.user_id}).exec((err, result)=> {
+        if(err) {
+            console.log(err);
+            next(err);
+        }
+        Account.update({_id: result.buyer}, {$pull: {buy_info: req.params.user_id}}).exec((err, result)=> {
+            if(err) {
+                console.log(err);
+                next(err);
+            }
+            BuyInfo.deleteOne({_id:req.params.user_id}).exec((err, result)=> {
+                if(err) {
+                    console.log(err);
+                    next(err);
+                }
+            });
+            res.redirect("../deposits");
+        });
+        
+    });
 });
 module.exports = router;
