51cf2eb37c3e3a46f596df20b743f31c3aefd2ca
juyonglee <juyonglee0208@gmail.com>
2019-02-10 15:53:53 +0900

    Coin 정보를 pdf로 출력하는 router 구현

--- COINPRA/routes/index.js
+++ COINPRA/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,6 +10,18 @@ router.get('/', function(req, res, next) {
   //   res.render('index', { title: 'Express'});
   // }
   res.render('test');
+});
+
+router.get('/PLUINFO', function(req, res, next){
+  var data =fs.readFileSync('public/pdf/PLU.pdf');
+  res.contentType("application/pdf");
+  res.send(data);
+});
+
+router.get('/TOXIINFO', function(req, res, next){
+  var data =fs.readFileSync('public/pdf/TOXI.pdf');
+  res.contentType("application/pdf");
+  res.send(data);
 });
 
 module.exports = router;
